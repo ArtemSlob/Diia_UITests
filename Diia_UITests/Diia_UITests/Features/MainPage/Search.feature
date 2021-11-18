c40c1d7951aff3527cbbcccc5e331cd8e1ba13a0
Desktop\Artem <artem.slobodianiuk@gmail.com>
2021-11-18 15:01:54 +0200

    Project upload

---
+++ Diia_UITests/Diia_UITests/Features/MainPage/Search.feature
@@ -0,0 +1,20 @@
+﻿@MainPage @Search
+Feature: Search
+	As a user
+	I want to search for information on the main page
+	In order to find needed information
+#
+#Background: 
+#	Given main page open
+#
+#@Valid
+#Scenario: Check the search results with valid input
+#	When I input 'Відкрити ФОП' in the search field
+#	When I click the search button
+#	Then I see a shearch result page whith text 'За вашим запитом знайдено матеріалів:'
+#
+#@Invalid
+#Scenario: Check the search with no result
+#	When I input 'invalid input 666' in the search field
+#	When I click the search button
+#	Then I see a shearch result page whith text 'За вашим запитом не знайдено матеріалів'
---
+++ Diia_UITests/Diia_UITests/POM/MainPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+
+namespace Diia_UITests.POM
+{
+    class MainPage
+    {
+        private readonly IWebDriver _webDriver;
+
+        public MainPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        private readonly By _servicesHeaderMenuDropDownLink = By.CssSelector("[data-menu-target='menu-sub-1']");
+        private readonly By _servicesHeaderMenuLinks = By.CssSelector("div[id='menu-sub-1'] [class='menu-sub_list-item diia-animated']>a");
+
+        public MainPage GoToMainPage()
+        {
+            _webDriver.Navigate().GoToUrl("https://diia.gov.ua/");
+            return this;
+        }
+
+        public void ClickOnServicesHeaderMenuDropDownLink()
+        {
+            _webDriver.FindElement(_servicesHeaderMenuDropDownLink).Click();
+        }
+
+        public void ClickOnServicesHeaderMenuFirstLink()
+        {
+            _webDriver.FindElements(_servicesHeaderMenuDropDownLink)[0].Click();
+        }
+
+        public string GetTextFromServicesHeaderMenuFirstLink()
+        {
+            return _webDriver.FindElements(_servicesHeaderMenuDropDownLink)[0].Text;
+        }
+    }
+}
