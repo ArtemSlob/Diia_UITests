1cb2764f0554d351809554abe07adf96d6e940d1
Artem <artem.slobodianiuk@gmail.com>
2021-11-21 18:57:21 +0200

    Some minor changes

--- Diia_UITests/Diia_UITests/POM/MainPage.cs
+++ Diia_UITests/Diia_UITests/POM/MainPage.cs
@@ -22,7 +22,7 @@ namespace Diia_UITests.POM
         private readonly By _servicesHeaderMenuDropDownLink = By.CssSelector("[data-menu-target='menu-sub-1']");
         private readonly By _servicesHeaderMenuLinks = By.CssSelector("div[id='menu-sub-1'] [class='menu-sub_list-item diia-animated']>a");
         private readonly By _servicePageTitle = By.CssSelector("h1");
-        private readonly By _serviceSection = By.CssSelector("[id='chatbot_btn']");
+        private readonly By _chatbotButton = By.CssSelector("[id='chatbot_btn']");
 
         public MainPage GoToMainPage()
         {
@@ -55,7 +55,7 @@ namespace Diia_UITests.POM
 
         public void ClickOnMainSection()
         {
-            _action.MoveToElement(_webDriver.FindElement(_serviceSection)).Click().Perform();
+            _action.MoveToElement(_webDriver.FindElement(_chatbotButton)).Click().Perform();
         }
 
         public string CheckActivenessOfMenu()
